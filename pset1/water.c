35db968cfde5f34277f604c6310ffe21f0bcfe01
Joh Yoshida <johyoshida@gmail.com>
2017-07-16 15:24:49 -0700

    Implement water.c

---
+++ pset1/water.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+
+// Report a user’s water usage, converting minutes spent in the shower to bottles of drinking water.
+int main(void){
+  // Prompt user for shower length in minutes (positive int)
+  printf("How many minutes do you shower for? ");
+  int minutes = GetInt();
+
+  // convert minutes to bottles of water
+  int bottles = m * 12;
+
+  // Print the equivalent number of bottles (positive int)
+  printf("Your shower used %i bottles of water!\n", bottles);
+}
